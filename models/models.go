d2bf814ee0c695a4a6501b9d220818b23db4eccb
DuramAsh <duramash.02@gmail.com>
2023-03-23 23:04:47 +0600

    get by id needs fix

--- models/models.go
+++ models/models.go
@@ -4,7 +4,11 @@ import "gorm.io/gorm"
 
 type Movie struct {
 	gorm.Model
-	Id     uint    `json:"id" gorm:"primaryKey"`
+	Title  string  `json:"title"`
+	Rating float64 `json:"rating"`
+}
+
+type APIMovie struct {
 	Title  string  `json:"title"`
 	Rating float64 `json:"rating"`
 }
